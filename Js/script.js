93046f2c45dd883ea86fb704c7caad1fd52b3cd9
atanuchina0709 <atanuchina07092002@gmail.com>
2025-12-16 14:34:55 +0530

    Initial Commit

---
+++ Js/script.js
@@ -0,0 +1,20 @@
+// Smooth scrolling for nav links
+document.querySelectorAll('a[href^="#"]').forEach((link) => {
+  link.addEventListener("click", function (e) {
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
+
+let navbar = document.getElementById("hero-nav");
+
+window.addEventListener("scroll", () => {
+  if (this.window.scrollY > 50) {
+    navbar.classList.add("nav-scrolled");
+  } else {
+    navbar.classList.remove("nav-scrolled");
+  }
+});
